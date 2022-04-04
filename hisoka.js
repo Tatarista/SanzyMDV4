348a243f3466224d2eec027da69f0b495dcb94e8
Rissbotz <101449752+Rissbotz@users.noreply.github.com>
2022-04-04 12:12:20 +0800

    Update hisoka.js

--- hisoka.js
+++ hisoka.js
@@ -3353,14 +3353,14 @@ case 'sewa': case 'sewabot': {
 ┃ 
 ┃▰▰▰▰▰▰▰▰▰▰▰▰▰▰
 ┃   ⬣ SEWA + PREM ⬣
-┃⬡ 1 MINGGU : 8K
-┃⬡ 1 BULAN : 18K
-┃⬡ PERMANEN : 25K
+┃⬡ 1 MINGGU : 5K
+┃⬡ 1 BULAN : 10K
+┃⬡ PERMANEN : Tanya owner
 ┃▰▰▰▰▰▰▰▰▰▰▰▰▰▰
 ┃       〔 KELEBIHAN BOT 〕
 ┃▰▰▰▰▰▰▰▰▰▰▰▰▰▰
 ┃⬡ ON 24 JAM
-┃⬡ RUN DI RDP
+┃⬡ RUN DI HEROKU
 ┃⬡ ANTI DELAY 
 ┃⬡ ANTILINK
 ┃⬡ WELCOME IMAGE
